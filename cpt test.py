419dbe299b9b078f9806108cb657f18d52a99cc3
zhao-qin <38043277+zhao-qin@users.noreply.github.com>
2018-05-30 10:18:06 -0400

    Add files via upload

---
+++ cpt test.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.init()
+black = (0,0,0)
+white = (255,255,255)
+size = (1280,720)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("welcome to zhao and eddies game")
